3de0606cb6b94c7ec43145f3f982812a058f6e8d
powcoder <powcoder@163.com>
2022-11-23 12:39:26 +0800

    CSE 231 Programming Project 08

---
+++ project.py
@@ -0,0 +1,68 @@
+https://powcoder.com
+代写代考加微信 powcoder
+Assignment Project Exam Help
+Add WeChat powcoder
+"""
+    SOURCE HEADER GOES HERE!
+"""
+
+import itertools
+from operator import itemgetter
+
+def open_file():
+    """
+    Insert function header
+    """
+    pass
+
+def read_file(fp):
+    """
+    Insert function header
+    """
+    pass
+
+def calculate_score(rack,word):
+    """
+    Insert function header
+    """
+    pass
+
+def generate_combinations(rack,placed_tile):
+    """
+    Insert function header
+    """
+    pass
+
+def generate_words(combo,scrabble_words_dict):    
+    """
+    Insert function header
+    """
+    pass
+
+def generate_words_with_scores(rack,placed_tile,scrabble_words_dict):
+    """
+    Insert function header
+    """
+    pass
+
+def sort_words(word_dic):
+    """
+    Insert function header
+    """
+    pass
+
+def display_words(word_list,specifier):
+    """
+    Insert function header
+    """
+    pass
+
+def main():
+    """
+    Insert function header
+    """
+    pass
+
+if __name__ == "__main__":
+    main()
+
